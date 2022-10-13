ff7c24df84d9e85a602342054f8d7ff724879b81
Ulna <g.alperenkurt@gmail.com>
2022-10-13 09:17:40 +0300

    project start

---
+++ src/components/employeeList/EmployeeList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./EmployeeList.css";
+
+const EmployeeList = () => {
+    return <div>EmployeeList</div>;
+};
+
+export default EmployeeList;
---
+++ src/components/paginateButtons/PaginateButtons.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./PaginateButtons.css";
+
+const PaginateButtons = () => {
+    return <div>PaginateButtons</div>;
+};
+
+export default PaginateButtons;
